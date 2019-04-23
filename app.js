e8cbfe11ecca5682dc62119ff062d1fd639458aa
Lzduque <lzduque@hotmail.com>
2019-04-23 18:12:30 +0000

    getting famous person

---
+++ app.js
@@ -0,0 +1,34 @@
+// const musicdb = require('./musicdb') // regular version
+// const musicdb = require('./musicdb') // promises version
+const musicdb = require('./live');
+
+// var artistName = process.argv[2];
+var albumName = process.argv[2];
+
+// See musicdb.js for function interface
+musicdb.tracksByAlbum(albumName, (rows) => {
+  console.log(`Found ${rows.length} tracks:\n`);
+  rows.forEach((row) => {
+    console.log('naked row ',row);
+    let output = [];
+    for (column in row) {
+      output.push(`${column}: ${row[column]}`);
+    }
+    console.log(output.join(', '));
+  });
+});
+
+// musicdb.allAlbums((result) => {
+//   console.log(result.rows)
+//   musicdb.finish()
+// })
+// musicdb.getAlbumTracks(artistName, albumName, (rows) => {
+//   console.log(`Found ${rows.length} tracks:\n`);
+//   rows.forEach((row) => {
+//     let output = [];
+//     for (column in row) {
+//       output.push(`${column}: ${row[column]}`);
+//     }
+//     console.log(output.join(', '));
+//   })
+// });
---
+++ lookup.js
@@ -0,0 +1,42 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+
+var artistName = process.argv[2];
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+
+  function artistsName (artistName, callback) {
+    client.query("SELECT * from famous_people where first_name = $1 or last_name = $1", [artistName], (err, res) => {
+            if (err) {
+              callback([]);
+            } else {
+              callback(res.rows);
+            }
+    });
+  }
+
+  artistsName(artistName, (rows) => {
+    console.log(`Found ${rows.length}person(s) by the name '${artistName}':\n`);
+    let count = 1;
+    rows.forEach((row) => {
+      let date = row.birthdate.getDate() + "-" + (row.birthdate.getMonth() + 1) + "-" + row.birthdate.getFullYear();
+      console.log(`- ${count}: ${row.first_name} ${row.last_name}, born '${date}'`);
+      count ++;
+    });
+  });
+
+
+});
